178ce8a26b495fdb7a3367bb495ae9eeb9477f97
vidighe <vishakha-babaji.a.dighe@capgemini.com>
2025-01-06 11:48:42 +0530

    Flat Buddies Initial configurations

---
+++ src/main/java/com/find/flat_buddies/FlatBuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.find.flat_buddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatBuddiesApplication.class, args);
+	}
+
+}
